c4d86fe513c822fe60888678be8e3073d5433789
Liao <vermouth0226@gmail.com>
2017-12-23 18:06:06 +0800

    cubeMove , Drop

--- TetrisModel.js
+++ TetrisModel.js
@@ -1,11 +1,30 @@
 var TetrisModel = function(){
+    
+    this.createMatrix = function(weight, height){
+        const matrix = [];
+        for(var i = 0; i < height; i++)
+            matrix.push(new Array(weight).fill(0));
+        return matrix;    
+    }
 
     this.isCollide = function(){
 
     }
 
-    this.cubeRotate = function(){
+    this.cubeRotate = function(matrix , dir){
+        
+    }
 
+    this.cubeDrop = function(player){
+        player.pos.y++;
+    }
+
+    this.cubeMove = function(player , dir){
+        player.pos.x +=dir;
+    }
+
+    this.quickDrop = function(player){
+        player.pos.y+=12;
     }
 
     this.arenaSweep = function(){
@@ -20,15 +39,60 @@ var TetrisModel = function(){
 
     }
 
-    this.cubeRandom = function(){
-
+    this.cubeRandom = function(type){
+         switch(type) {
+            case 'T':
+                return [
+                    [0, 0, 0],
+                    [1, 1, 1],
+                    [0, 1, 0],
+                ];
+            case 'O':
+                return [
+                    [1, 1],
+                    [1, 1],
+                ];
+            case 'L':
+                return [
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [0, 1, 1],
+                ];
+            case 'J':
+                return [
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [1, 1, 0],
+                ];
+            case 'I':
+                return [
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                ];
+            case 'Z':
+                return [
+                    [1, 1, 0],
+                    [0, 1, 1],
+                    [0, 0, 0],
+                ];
+            case 'S':
+                return [
+                    [0, 1, 1],
+                    [1, 1, 0],
+                    [0, 0, 0],
+                ];
+        }
     }
+
+    
 
     this.drawMatrix = function(context, matrix, offset){
         matrix.forEach((row, y) => {
             row.forEach((value, x) =>{
                 if (value !== 0) {
-                    context.fillStyle = 'black';
+                    context.fillStyle = 'red'//colors[value] ;
                     context.fillRect(x + offset.x, y + offset.y, 1, 1);
                 }
             });
--- TetrisView.js
+++ TetrisView.js
@@ -3,12 +3,15 @@ var TetrisView = function(model){
 	var canvas = document.getElementById('tetris');
     var context = canvas.getContext('2d');
     context.scale(20, 20);
+    var dropCounter = 0;
+    var dropInterval = 1000;
+    var lastTime = 0;
     
-    matrix = [
+    /*matrix = [
     	[0, 0, 0],
         [1, 1, 1],
         [0, 1, 0],
-    ]
+    ]*/
 
     const player = {
         pos: {x: 5, y: 5},
@@ -16,16 +19,34 @@ var TetrisView = function(model){
 	}
 
 	this.draw = function(){
-        context.fillStyle = "rgba(0, 0, 0, 0)";
+        context.fillStyle = "#222";//"rgba(0, 0, 0, 0)";
         context.fillRect(0, 0, canvas.width, canvas.height);
         tetrismodel.drawMatrix(context, player.matrix, player.pos);
     }
 
     this.getUserKeyDown = function(){
-
+        if(event.keyCode===39) //right
+            tetrismodel.cubeMove(player , 1);
+        else if(event.keyCode===37) //left
+            tetrismodel.cubeMove(player , -1);
+        else if(event.keyCode===40) {//down
+            tetrismodel.cubeDrop(player);
+            dropCounter = 0;
+        }
+        else if(event.keyCode===38) //rotate
+            tetrismodel.cubeRotate(player.matrix , 1);
     }
 
     this.updateGameView = function(time = 0){
+        const deltaTime = time -lastTime;
+        lastTime = time;
+        dropCounter += deltaTime;
+        if(dropCounter > dropInterval){
+            tetrismodel.cubeDrop(player);
+            dropCounter = 0;
+        }
+
+        //console.log(deltaTime);
         var self = this;
         self.draw();
         requestAnimationFrame(self.updateGameView.bind(self));
@@ -34,6 +55,5 @@ var TetrisView = function(model){
     this.setting = function(){
 
     }
-
 };
 
